dbad27951b4bcee8082a9f96885aca2e069d44c4
Sahil Mohril <sahilmohril@gmail.com>
2025-03-27 17:34:10 +0530

    Update Calendar.js

--- Calendar.js
+++ Calendar.js
@@ -6,6 +6,8 @@ export default function Calendar() {
     const [month, setMonth] = useState(new Date().getMonth());
     const [year, setYear] = useState(new Date().getFullYear());
     const [monthname, setMonthName] = useState(["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
+    const [openMenu, setOpenMenu] = useState([false, false, false, false, false])
+    const [dateopen, setDateOpen] = useState(0);
     useEffect(() => { getDates(month, year) }, [month, year])
 
     function getDates() {
@@ -42,6 +44,11 @@ export default function Calendar() {
     function NextMonth() {
         setYear(month + 1 == 12 ? year + 1 : year)
         setMonth(month + 1 == 12 ? 0 : month + 1)
+        let open = [...openMenu]
+        for (let i = 0; i < open.length; i++)
+            open[i] = false;
+        console.log(open);
+        setOpenMenu(open);
         // console.log(month);
         // console.log(year);
 
@@ -53,8 +60,24 @@ export default function Calendar() {
     function PrevMonth() {
         setMonth(month - 1 == -1 ? 11 : month - 1)
         setYear(month - 1 == -1 ? year - 1 : year);
+        let open = [...openMenu]
+        for (let i = 0; i < open.length; i++)
+            open[i] = false;
+        console.log(open);
+        setOpenMenu(open);
     }
+    function dateClick(e) {
+        const week = ['Week1', 'Week2', 'Week3', 'Week4', 'Week5', 'Week6']
+        //console.log(e.target.classList[3]);
+        //console.log(week.indexOf(e.target.classList[3]))
+        let open = [...openMenu];
+        for (let i = 0; i < open.length; i++)
+            open[i] = false;
+        open[week.indexOf(e.target.classList[3])] = !open[week.indexOf(e.target.classList[3])];
+        setOpenMenu(open);
+        setDateOpen(e.target.textContent)
 
+    }
 
     return (<div>
         <div className="Month">
@@ -73,17 +96,17 @@ export default function Calendar() {
             <div className="Day">Saturday</div>
             <div className="Day">Sunday</div>
             <div className="Week">Week1</div>
-            <div className="Date R1 C1 Week1">{dates[0]}</div>
-            <div className="Date R1 C2 Week1">{dates[1]}</div>
-            <div className="Date R1 C3 Week1">{dates[2]}</div>
-            <div className="Date R1 C4 Week1">{dates[3]}</div>
-            <div className="Date R1 C5 Week1">{dates[4]}</div>
-            <div className="Date R1 C6 Week1">{dates[5]}</div>
-            <div className="Date R1 C7 Week1">{dates[6]}</div>
-            <div className="Week-menu Week1 show">
+            <div className="Date R1 C1 Week1" onClick={(e) => dateClick(e)}>{dates[0]}</div>
+            <div className="Date R1 C2 Week1" onClick={(e) => dateClick(e)}>{dates[1]}</div>
+            <div className="Date R1 C3 Week1" onClick={(e) => dateClick(e)}>{dates[2]}</div>
+            <div className="Date R1 C4 Week1" onClick={(e) => dateClick(e)}>{dates[3]}</div>
+            <div className="Date R1 C5 Week1" onClick={(e) => dateClick(e)}>{dates[4]}</div>
+            <div className="Date R1 C6 Week1" onClick={(e) => dateClick(e)}>{dates[5]}</div>
+            <div className="Date R1 C7 Week1" onClick={(e) => dateClick(e)}>{dates[6]}</div>
+            {openMenu[0] && (<div className="Week-menu Week1">
                 <div className=" today">
-                    <p className="selecteddate">14th January</p>
-                    <p className="selectedyear">2025</p>
+                    <p className="selecteddate">{dateopen}th {monthname[month]}</p>
+                    <p className="selectedyear">{year}</p>
                 </div>
                 <div className="meal-info">
                     <div className="Meal1">Breakfast</div>
@@ -110,19 +133,19 @@ export default function Calendar() {
 
 
                 <div className="add">+</div>
-            </div>
+            </div>)}
             <div className="Week">Week2</div>
             <div className="Date R2 C1 Week2">{dates[7]}
             </div>
             <div className="Date R2 C2 Week2">{dates[8]}
                 <div className="summary">Breakfast</div>
             </div>
-            <div className="Date R2 C3 Week2">{dates[9]}</div>
-            <div className="Date R2 C4 Week2">{dates[10]}</div>
-            <div className="Date R2 C5 Week2">{dates[11]}</div>
-            <div className="Date R2 C6 Week2">{dates[12]}</div>
-            <div className="Date R2 C7 Week2">{dates[13]}</div>
-            <div className="Week-menu Week2 show">
+            <div className="Date R2 C3 Week2" onClick={(e) => dateClick(e)}>{dates[9]}</div>
+            <div className="Date R2 C4 Week2" onClick={(e) => dateClick(e)}>{dates[10]}</div>
+            <div className="Date R2 C5 Week2" onClick={(e) => dateClick(e)}>{dates[11]}</div>
+            <div className="Date R2 C6 Week2" onClick={(e) => dateClick(e)}>{dates[12]}</div>
+            <div className="Date R2 C7 Week2" onClick={(e) => dateClick(e)}>{dates[13]}</div>
+            {openMenu[1] && (<div className="Week-menu Week2 ">
                 <div className="today">
                     <p className="selecteddate">14th January</p>
                     <p className="selectedyear">2025</p>
@@ -158,16 +181,16 @@ export default function Calendar() {
                 </div>
 
                 <div className="add">+</div>
-            </div>
+            </div>)}
             <div className="Week">Week3</div>
-            <div className="Date R3 C1 Week3">{dates[14]}</div>
-            <div className="Date R3 C2 Week3">{dates[15]}</div>
-            <div className="Date R3 C3 Week3">{dates[16]}</div>
-            <div className="Date R3 C4 Week3">{dates[17]}</div>
-            <div className="Date R3 C5 Week3">{dates[18]}</div>
-            <div className="Date R3 C6 Week3">{dates[19]}</div>
-            <div className="Date R3 C7 Week3">{dates[20]}</div>
-            <div className="Week-menu Week3 show">
+            <div className="Date R3 C1 Week3" onClick={(e) => dateClick(e)}>{dates[14]}</div>
+            <div className="Date R3 C2 Week3" onClick={(e) => dateClick(e)}>{dates[15]}</div>
+            <div className="Date R3 C3 Week3" onClick={(e) => dateClick(e)}>{dates[16]}</div>
+            <div className="Date R3 C4 Week3" onClick={(e) => dateClick(e)}>{dates[17]}</div>
+            <div className="Date R3 C5 Week3" onClick={(e) => dateClick(e)}>{dates[18]}</div>
+            <div className="Date R3 C6 Week3" onClick={(e) => dateClick(e)}>{dates[19]}</div>
+            <div className="Date R3 C7 Week3" onClick={(e) => dateClick(e)}>{dates[20]}</div>
+            {openMenu[2] && (<div className="Week-menu Week3">
                 <div className="today">
                     <p className="selecteddate">14th January</p>
                     <p className="selectedyear">2025</p>
@@ -197,16 +220,16 @@ export default function Calendar() {
 
 
                 <div className="add">+</div>
-            </div>
+            </div>)}
             <div className="Week">Week4</div>
-            <div className="Date R4 C1 Week4">{dates[21]}</div>
-            <div className="Date R4 C2 Week4">{dates[22]}</div>
-            <div className="Date R4 C3 Week4">{dates[23]}</div>
-            <div className="Date R4 C4 Week4">{dates[24]}</div>
-            <div className="Date R4 C5 Week4">{dates[25]}</div>
-            <div className="Date R4 C6 Week4">{dates[26]}</div>
-            <div className="Date R4 C7 Week4">{dates[27]}</div>
-            <div className="Week-menu Week4 show">
+            <div className="Date R4 C1 Week4" onClick={(e) => dateClick(e)}>{dates[21]}</div>
+            <div className="Date R4 C2 Week4" onClick={(e) => dateClick(e)}>{dates[22]}</div>
+            <div className="Date R4 C3 Week4" onClick={(e) => dateClick(e)}>{dates[23]}</div>
+            <div className="Date R4 C4 Week4" onClick={(e) => dateClick(e)}>{dates[24]}</div>
+            <div className="Date R4 C5 Week4" onClick={(e) => dateClick(e)}>{dates[25]}</div>
+            <div className="Date R4 C6 Week4" onClick={(e) => dateClick(e)}>{dates[26]}</div>
+            <div className="Date R4 C7 Week4" onClick={(e) => dateClick(e)}>{dates[27]}</div>
+            {openMenu[3] && (<div className="Week-menu Week4">
                 <div className="today">
                     <p className="selecteddate">14th January</p>
                     <p className="selectedyear">2025</p>
@@ -236,15 +259,15 @@ export default function Calendar() {
 
 
                 <div className="add">+</div>
-            </div>
+            </div>)}
             <div className="Week">Week5</div>
-            <div className="Date R5 C1 Week5">{dates[28]}</div>
-            <div className="Date R5 C2 Week5">{dates[29]}</div>
-            <div className="Date R5 C3 Week5">{dates[30]}</div>
-            <div className="Date R5 C4 Week5">{dates[31]}</div>
-            <div className="Date R5 C5 Week5">{dates[32]}</div>
-            <div className="Date R5 C6 Week5">{dates[33]}</div>
-            <div className="Date R5 C7 Week5">{dates[34]}</div>
+            <div className="Date R5 C1 Week5" onClick={(e) => dateClick(e)}>{dates[28]}</div>
+            <div className="Date R5 C2 Week5" onClick={(e) => dateClick(e)}>{dates[29]}</div>
+            <div className="Date R5 C3 Week5" onClick={(e) => dateClick(e)}>{dates[30]}</div>
+            <div className="Date R5 C4 Week5" onClick={(e) => dateClick(e)}>{dates[31]}</div>
+            <div className="Date R5 C5 Week5" onClick={(e) => dateClick(e)}>{dates[32]}</div>
+            <div className="Date R5 C6 Week5" onClick={(e) => dateClick(e)}>{dates[33]}</div>
+            <div className="Date R5 C7 Week5" onClick={(e) => dateClick(e)}>{dates[34]}</div>
             <div className="Week-menu Week5 show">
                 <div className="today">
                     <p className="selecteddate">14th January</p>
@@ -253,13 +276,13 @@ export default function Calendar() {
                 <div className="add">+</div>
             </div>
             <div className="Week">Week6</div>
-            <div className="Date R6 C1 Week6">{dates[35]}</div>
-            <div className="Date R6 C2 Week6">{dates[36]}</div>
-            <div className="Date R6 C3 Week6">{dates[37]}</div>
-            <div className="Date R6 C4 Week6">{dates[38]}</div>
-            <div className="Date R6 C5 Week6">{dates[39]}</div>
-            <div className="Date R6 C6 Week6">{dates[40]}</div>
-            <div className="Date R6 C7 Week6">{dates[41]}</div>
+            <div className="Date R6 C1 Week6" onClick={(e) => dateClick(e)}>{dates[35]}</div>
+            <div className="Date R6 C2 Week6" onClick={(e) => dateClick(e)}>{dates[36]}</div>
+            <div className="Date R6 C3 Week6" onClick={(e) => dateClick(e)}>{dates[37]}</div>
+            <div className="Date R6 C4 Week6" onClick={(e) => dateClick(e)}>{dates[38]}</div>
+            <div className="Date R6 C5 Week6" onClick={(e) => dateClick(e)}>{dates[39]}</div>
+            <div className="Date R6 C6 Week6" onClick={(e) => dateClick(e)}>{dates[40]}</div>
+            <div className="Date R6 C7 Week6" onClick={(e) => dateClick(e)}>{dates[41]}</div>
             <div className="Week-menu Week6 show">
                 <div className="today">
                     <p className="selecteddate">14th January</p>
